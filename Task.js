bc44de27bc67435462d1df502cb1bbb2ba5a2eb3
Apolgasim <81713835+ALSHAZLI@users.noreply.github.com>
2021-12-27 15:58:28 +0300

    Update Task.js

--- Task.js
+++ Task.js
@@ -1,54 +1,70 @@
 const express = require ('express');
 
 const app = express();
-var axios = require('axios');
+const request = require("request")
 
-app.get('/', (req, res) => {
+
+function apiCall (reqOps) {
+    return new Promise ( (resolve, reject) => {
+
+        request(reqOps, (err, res, body) => {
+
+            if(!err && res.statusCode == 200){
+                resolve( JSON.parse(body) );                
+            }
+
+            reject(err);
+        });
+
+    });
+}
+
 
 // get city name by lat and long in arabic language
 
-var config1 = {
+var configCity = {
     method: 'get',
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?latlng=15.59040388313972,32.54970895180953&language=ar&key=AIzaSyCfM-A7MhbvNM3h99uqRl_E-jouII9SJGA',
+    url: 'https://maps.googleapis.com/maps/api/geocode/json?latlng=15.59040388313972,32.54970895180953&language=ar&key=AIzaSyB09k6jqR4ncnhxQ_5DMl1JorQCPoi6IMw',
+    headers: { }
+  };
+  var configNear = {
+    method: 'get',  
+    url: 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=15.69040388313972,32.64970895180953&language=ar&radius=50&key=AIzaSyB09k6jqR4ncnhxQ_5DMl1JorQCPoi6IMw',
     headers: { }
   };
 
-  axios(config1)
+  app.get("/", function(req, res){
+
+
+    let dataCity, dataNear;
+  
+  apiCall(configCity)
+
+//   apiCall(config1)
 
   .then( async function (response) {
-      const task2 = await JSON.stringify(response.data.results[0].address_components[1].short_name);
+      // get city name by lat and long in arabic language
+      const task2 = await JSON.stringify(response.results[0].address_components[1].short_name);
+      dataCity = task2;
       console.log(task2);
-      res.send(`City Name From Lat And Long ${task2}`);
+      return apiCall(configNear);
     })
-    .catch(function (error) {
-      console.log(error);
-    }); 
-  })
-
-  app.get('/task', (req, res) => {
-
-    // get city name by lat and long in arabic language
     
-    var config = {
-        method: 'get',  
-        url: 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=15.59040388313972,32.54970895180953&language=ar&radius=1500&key=AIzaSyCfM-A7MhbvNM3h99uqRl_E-jouII9SJGA',
-        headers: { }
-      };
-    
-      axios(config)
-    
-   
 .then( async function (response) {
-    const task3 = await JSON.stringify(response.data.results[0].name);
+    // get NearBy Place  for same lat long in arabic language
+    const task3 = await JSON.stringify(response.results[0].name);
     console.log(task3);
-    res.send(`the nearby place of the same lat and long in get city name ${task3}`);
+    dataNear = task3;
+    console.log(dataCity,dataNear);
+    res.send(`${dataCity}, ${dataNear}`)
+    
   })
   .catch(function (error) {
     console.log(error);
   });
-        }); 
+        
       
-
+  })
 
 app.listen((process.env.PORT|| 5000), ()=>{
     console.log('Backend server running on port 5000')
