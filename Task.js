e0c2df4194114e8249dfdf73fe503f8de3772656
Apolgasim <81713835+ALSHAZLI@users.noreply.github.com>
2021-12-31 23:42:50 +0300

    Update Task.js

--- Task.js
+++ Task.js
@@ -7,6 +7,7 @@ const request = require("request")
 function apiCall (reqOps) {
     return new Promise ( (resolve, reject) => {
 
+       
         request(reqOps, (err, res, body) => {
 
             if(!err && res.statusCode == 200){
@@ -24,35 +25,50 @@ function apiCall (reqOps) {
 
 var configCity = {
     method: 'get',
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?latlng=15.689549289692504,32.50204974742722&language=ar&key=AIzaSyB09k6jqR4ncnhxQ_5DMl1JorQCPoi6IMw',
+    url: 'https://maps.googleapis.com/maps/api/geocode/json?latlng=18.110826080251847,-15.952946587972686&language=ar&key=AIzaSyCtlLNJuEdOotatX1PBGiG7OoooEQ92RXU',
     headers: { }
   };
   var configNear = {
     method: 'get',  
-    url: 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=15.689549289692504,32.50204974742722&language=ar&radius=500&key=AIzaSyB09k6jqR4ncnhxQ_5DMl1JorQCPoi6IMw',
+    url: 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=18.110826080251847,-15.952946587972686&language=ar&radius=500&key=AIzaSyCtlLNJuEdOotatX1PBGiG7OoooEQ92RXU',
     headers: { }
   };
 
   app.get("/", function(req, res){
 
 
-    let dataCity, dataNear;
+    let dataCity, dataNear, dataReigon, dataCountry;
   
   apiCall(configCity)
     .then( async function (response) {
         // get city name by lat and long in arabic language 
-        const task2 = await JSON.stringify(response.results[0].address_components[1].short_name);
-        dataCity = task2;
-        console.log(task2);
+       
+        for (var i=0; i<response.results[0].address_components.length; i++)
+            {
+               
+                if(response.results[i].address_components[0].types[0] == "neighborhood"){
+                        const   region = response.results[i].address_components[0].long_name;
+                        console.log(region);
+                        dataReigon = region;
+                    }else if(dataReigon == undefined){
+                        if(response.results[i].address_components[1].types[2] == "sublocality_level_1"){
+                            const   region = response.results[i].address_components[1].long_name;
+                            console.log(region);
+                            dataReigon = region;
+                        }
+                    }
+            }
+
+        
         return apiCall(configNear);
     })
-    
     .then( async function (response) {
         // get NearBy Place  for same lat long in arabic language
     
-        for (let i = 0; i <= 2; ++i) {
-        
+        for (let i = 0; i <= 10; ++i) {
+            
             const task4 = await JSON.stringify(response.results[i].name);
+            
             console.log(task4);
        
             dataNear = task4;
@@ -60,8 +76,12 @@ var configCity = {
 
    
     
-        console.log(dataCity,dataNear);
-        res.send(`${dataCity}, ${dataNear}`)
+        console.log(dataNear);
+        console.log(dataReigon);
+      // هنا لو عايز اللرسبونس على شكل جييسون json
+//         res.status(200).json(`${dataReigon},${dataNear}`);
+       res.send(`${dataReigon},${dataNear}`);
+        
     
     })
     .catch(function (error) {
